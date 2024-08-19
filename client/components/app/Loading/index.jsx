fae183429783af71f3f29950ffd801780aacca8a
lareii <lareii@proton.me>
2024-08-19 17:29:49 +0300

    refactor: remove unused code

--- client/components/app/Loading/index.jsx
+++ client/components/app/Loading/index.jsx
@@ -3,24 +3,20 @@
 import { LoaderCircle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
-import useLoadingStore from '@/stores/loading';
 
 export default function Loading() {
-  const loading = useLoadingStore((state) => state.loading);
-
   return (
     <AnimatePresence mode='wait'>
-      {loading && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ease: 'easeInOut', duration: 0.3 }}
-          className='h-screen flex flex-col items-center justify-center'
-        >
-          <Image src='/copluk.png' alt='copl.uk logo' width={170} height={170} className='mb-7' />
-          <LoaderCircle className='w-6 h-6 animate-spin' />
-        </motion.div>
-      )}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: 'easeInOut', duration: 0.3 }}
+        className='h-screen flex flex-col items-center justify-center'
+      >
+        <Image src='/copluk.png' alt='copl.uk logo' width={170} height={170} className='mb-7' />
+        <LoaderCircle className='w-6 h-6 animate-spin' />
+      </motion.div>
     </AnimatePresence>
   );
 }
